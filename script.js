7ebc9f84a3a55afd96f280feabadf692cb37e9d7
Jordan Chalupka <jchalupk@mail.uoguelph.ca>
2016-04-14 19:11:13 -0400

    updated

---
+++ script.js
@@ -0,0 +1,15 @@
+
+/*
+var d = new Date();
+
+var day = d.getDate();
+var month = d.getMonth()+1;
+var year = d.getFullYear();
+
+document.getElementById("foot01").innerHTML =
+"<p>Last Updated: " + month + "/" + day + "/" + year +"</p>";
+*/
+
+function showAlert () {
+	alert("Hello World");
+}
